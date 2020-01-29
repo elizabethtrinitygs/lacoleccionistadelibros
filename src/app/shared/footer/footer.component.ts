87e98e45860389114eb8b1961dcaaeee2fb8654a
trinitysilvia <trinitysilviag@gmail.com>
2020-01-28 22:40:15 -0600

    archivo footer.component.ts: agregando linea para generador automatico de año

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  year: number = new Date().getFullYear();
+
+
   constructor() { }
 
   ngOnInit() {
