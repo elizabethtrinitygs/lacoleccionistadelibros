a9194926805c9f724d156da80398655095d5d08d
trinitysilvia <trinitysilviag@gmail.com>
2020-01-28 23:42:19 -0600

    definiendo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArchivoComponent } from './pages/archivo/archivo.component';
+import { SobremiComponent } from './pages/sobremi/sobremi.component';
+import { ElementoComponent } from './pages/elemento/elemento.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ArchivoComponent },
+  { path: 'sobre mi', component: SobremiComponent },
+  { path: 'elemento', component: ElementoComponent },
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
