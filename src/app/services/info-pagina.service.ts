67051536236c10ae3bd65a7443610a970f73b4ce
trinitysilvia <trinitysilviag@gmail.com>
2020-01-30 00:45:49 -0600

    llamdn oel servicio y accediendo a un dato especifico, se vizualiza en consola , prueba

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  constructor() {
+  constructor( private http: HttpClient) {
+
     console.log('Servicio de data exitoso');
 // leer el archivo JSON
+this.http.get('assets/data/data-pagina.json')
+.subscribe( resp => {
+  // console.log(resp);
+  console.log( resp['twitter']);
+})
 
    }
 }
