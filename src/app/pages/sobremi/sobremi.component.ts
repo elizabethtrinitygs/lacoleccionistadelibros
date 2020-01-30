2391f341324d691cead9c2e10240070ec80dabcf
trinitysilvia <trinitysilviag@gmail.com>
2020-01-30 16:48:31 -0600

    mostrando la informacion de firebase en el html, eliminamos las tarjetas del html de la seccion --sobre mi-

--- src/app/pages/sobremi/sobremi.component.ts
+++ src/app/pages/sobremi/sobremi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-sobremi',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobremiComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _infoService: InfoPaginaService) { }
 
   ngOnInit() {
   }
