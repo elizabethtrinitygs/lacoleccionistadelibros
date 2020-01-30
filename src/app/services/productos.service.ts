7fbe56354f2d83620e4485b91d71fcfec0433626
trinitysilvia <trinitysilviag@gmail.com>
2020-01-30 17:29:54 -0600

    creando servicio para cargar los productos

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {}
 }
