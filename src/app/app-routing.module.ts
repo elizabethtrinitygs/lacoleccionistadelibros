eeb870a262906215dfac1e437b7d82679362254f
trinitysilvia <trinitysilviag@gmail.com>
2020-02-01 00:43:13 -0600

    diseño y filtro de la pagina de buscqueda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ArchivoComponent } from './pages/archivo/archivo.component';
 import { SobremiComponent } from './pages/sobremi/sobremi.component';
 import { ElementoComponent } from './pages/elemento/elemento.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
 const routes: Routes = [
   { path: 'archivo', component: ArchivoComponent },
   { path: 'sobremi', component: SobremiComponent },
   { path: 'elemento/:id', component: ElementoComponent },
+  { path: 'searh/:termino', component: SearchComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'archivo'}
 ];
 
